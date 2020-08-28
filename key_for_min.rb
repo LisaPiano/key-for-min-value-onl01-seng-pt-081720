c658f04668db16a54e8a7620a17a4a25c7033593
Lisa Walters <campi_l@fortlewis.edu>
2020-08-28 21:58:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,19 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+def key_for_min_value(ikea)
+  if ikea[:chair] < ikea[:table] && ikea[:chair] < ikea[:mattress]
+      puts "chair"
+      elsif ikea[:table] < ikea[:chair] && ikea[:table] < ikea[:mattress]
+      puts "table"
+    
+    else 
+      puts "mattress"
+      
+  
+  
+  
+  end
+  end
 
 end
